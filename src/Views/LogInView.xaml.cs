2cfe412868a6b3aaf911d6273fb679117c9a5a96
Igor Morenec <Igor.Morenec@gmail.com>
2018-10-15 18:36:18 +0300

    Changed LogInView to be a UserControl. Implemented View and ViewModel binding. Now displaying MainWindow and LogInView in it. Changed password in LogInVM to be a SecureString. Moved App.xaml to outer (src) scope.

--- src/Views/LogInView.xaml.cs
+++ src/Views/LogInView.xaml.cs
@@ -1,12 +1,22 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+
+using AlarmClock.ViewModels;
 
 namespace AlarmClock.Views
 {
-    public partial class LogInView : Window
+    public partial class LogInView : UserControl
     {
         public LogInView()
         {
             InitializeComponent();
+            DataContext = new LogInViewModel();
+        }
+
+        private void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext != null)
+                ((dynamic)DataContext).Password = ((PasswordBox)sender).SecurePassword;
         }
     }
 }
