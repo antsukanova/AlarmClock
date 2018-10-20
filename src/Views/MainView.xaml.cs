2e862e0ca0bf515f3a4059a63721404572c6cf5b
Annabell <Ts.Nyutka@gmail.com>
2018-10-20 15:48:35 +0300

    Add all basic icons for user, log out from main app, for add, delete and ring clocks Add logo app Updated RelayCommand.cs (from Igor's last commit) Add MainViewModel.cs for processing events and adding some items on the main window Modify MainView.xaml with basic clock alarm model(current time, pattern for session current user, choosing hours and minutes with validation) In csproj changed version of framework and add icon for application and add recources for pictures

--- src/Views/MainView.xaml.cs
+++ src/Views/MainView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using AlarmClock.ViewModels;
+using System;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace AlarmClock.Views
 {
@@ -7,7 +10,21 @@ namespace AlarmClock.Views
         public MainView()
         {
             InitializeComponent();
-            //DataContext = new MainViewModel();
+            DataContext = new MainViewModel();
+        }
+
+        private void KeyDownHandler(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            int myCaretIndex = ((TextBox)sender).CaretIndex;
+            char[] characters = ((TextBox)sender).Text.ToCharArray();
+
+            if (myCaretIndex < characters.Length)
+            {
+                characters[myCaretIndex] = (Char)KeyInterop.VirtualKeyFromKey(e.Key);
+                ((TextBox)sender).Text = string.Join("", characters);
+                ((TextBox)sender).CaretIndex = myCaretIndex + 1;
+                e.Handled = true;
+            }
         }
     }
 }
