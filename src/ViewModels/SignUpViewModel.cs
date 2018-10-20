5297eeaea3fe0d92d368a9cca105d9aaee5ebbe9
Igor Morenec <Igor.Morenec@gmail.com>
2018-10-20 11:02:46 +0300

    Added empty SignUpViewModel and connected it to the view.

---
+++ src/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+using AlarmClock.Annotations;
+using AlarmClock.Managers;
+using AlarmClock.Misc;
+using AlarmClock.Models;
+using AlarmClock.Properties;
+using AlarmClock.Repositories;
+
+namespace AlarmClock.ViewModels
+{
+    class SignUpViewModel : INotifyPropertyChanged
+    {
+        #region properties
+        
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
