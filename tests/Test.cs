95baf19c261ad0ab3bdb3b69a94cf47ac7c8096d
Igor Morenec <Igor.Morenec@gmail.com>
2018-10-26 15:13:06 +0300

    Added FluentAssertions for convenience. Updated example

--- tests/Test.cs
+++ tests/Test.cs
@@ -1,4 +1,5 @@
 ﻿
+using FluentAssertions;
 using Xunit;
 
 namespace AlarmClock.tests
@@ -6,15 +7,11 @@ namespace AlarmClock.tests
     public class Test
     {
         [Fact]
-        public void Tst()
-        {
-            Assert.Equal(1, 2 - 1);
-        }
+        public void Tst() => Add(1, 2).Should().Be(1 + 2, "Math");
 
         [Fact]
-        public void Fail()
-        {
-            Assert.Equal(1, 2);
-        }
+        public void Fail() => Add(1, 5).Should().NotBe(1 - 5, "Math!");
+
+        public int Add(int a, int b) => a + b;
     }
 }
