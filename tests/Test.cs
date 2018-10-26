1280f2879d26da35323fa8aa588f7ccc793c69d8
Igor Morenec <Igor.Morenec@gmail.com>
2018-10-26 15:02:00 +0300

    Added xUnit to the project and created dummy test for demostration

---
+++ tests/Test.cs
@@ -0,0 +1,20 @@
+﻿
+using Xunit;
+
+namespace AlarmClock.tests
+{
+    public class Test
+    {
+        [Fact]
+        public void Tst()
+        {
+            Assert.Equal(1, 2 - 1);
+        }
+
+        [Fact]
+        public void Fail()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
